ea71c5029aa2ce318b7f2a93de9135c2486f59c1
Sandip Chavda <sandip.techultra@gmail.com>
2025-12-16 23:25:14 +0530

    Prepare chatter_datetime_hco module for Odoo Apps publication

--- chatter_datetime_hco/__manifest__.py
+++ chatter_datetime_hco/__manifest__.py
@@ -1,14 +1,20 @@
 {
     'name': "Chatter Datetime",
-    'summary': """
-        Displays the exact date and time in the chatter instead of showing relative days.
-        Helps users track messages with precise timestamps.
-    """,
+    'version': '18.0.0.0.0',
+    'summary': "Displays the exact date and time in the chatter instead of showing relative days.",
+    'description': """
+Chatter Datetime module enhances Odoo's messaging system by showing precise timestamps in the chatter. 
+This helps users track messages with exact date and time, instead of relative time (like '2 days ago').
+
+Features:
+- Displays exact date and time in chatter messages
+- Improves traceability of communication
+- Compatible with Odoo 18
+""",
     'author': "HCOdooTech",
     'website': "https://www.linkedin.com/in/hcodoo-tech-191311391",
-    'category': 'Productivity/Discuss',
+    'category': 'Productivity',
     'images': ['static/description/banner.png'],
-    'version': '18.0.0.0',
     'depends': ['mail'],
     'price': 3.99,
     'currency': 'USD',
@@ -18,6 +24,8 @@
         ]
     },
     'installable': True,
+    'application': True,
     'auto_install': False,
     'license': 'OPL-1',
 }
+
